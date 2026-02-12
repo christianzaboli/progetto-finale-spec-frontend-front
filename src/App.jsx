c52035fa40e7499744bf1354d6f07535d42f5f6f
osaze <osazezaboli@gmail.com>
2026-02-12 18:33:42 +0100

    ordinamento A-Z basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import GlobalProvider from "./contexts/GlobalContext";
+import { Navigate, Route, Routes } from "react-router-dom";
 // pagine
 import Homepage from "./pages/Homepage";
 import ServiceDetail from "./pages/ServiceDetail";
 function App() {
   return (
     <>
-      <GlobalProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="/service/:slug" element={<ServiceDetail />} />
-          </Routes>
-        </BrowserRouter>
-      </GlobalProvider>
+      <Routes>
+        <Route path="/" element={<Navigate to={"/homepage"} />} />
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/services/:slug" element={<ServiceDetail />} />
+      </Routes>
     </>
   );
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,18 @@
 import { useMemo, useState } from "react";
 import { useGlobalContext } from "../contexts/GlobalContext";
 export default function Homepage() {
+  // import dati
   const { services, categories } = useGlobalContext();
+
+  // filtri
   const [titleSearch, setTitleSearch] = useState("");
   const [categoryFilter, setCategoryFilter] = useState("");
+
+  // ordinamento
+  const [sortOrder, setSortOrder] = useState({
+    order: 1,
+    text: "Ordina dalla A-Z",
+  });
 
   const memoedServices = useMemo(() => {
     // filtro servizi in base alla ricerca input
@@ -12,26 +21,35 @@ export default function Homepage() {
         ? true
         : s.title.toLocaleLowerCase().includes(titleSearch.toLocaleLowerCase()),
     );
+    const chosenCategory = categories.find((c) => c === categoryFilter);
+
+    //ordimaneto a-z z-a per titolo (da ampliare)
+    filteredServices.sort(
+      (a, b) => a.title.localeCompare(b.title) * sortOrder.order,
+    );
 
     // filtro servizi (gia filtrati per input) in base alle categorie (evitando hardcode di generi in caso ce ne fossero nuovi)
     if (categoryFilter !== "") {
-      const chosenCategory = categories.find((c) => c === categoryFilter);
       return filteredServices.filter((s) => s.category === chosenCategory);
     }
 
     return filteredServices;
-  }, [services, titleSearch, categoryFilter]);
+  }, [services, titleSearch, categoryFilter, sortOrder]);
 
-  // render zone --------------------------------
+  // RENDER ZZZZZONE --------------------------------
   return (
     <>
       <h1>Benvenuto nel comparatore!</h1>
+
+      {/* ricerca */}
       <input
         type="text"
         placeholder="Cerca il titolo della piattaforma..."
         value={titleSearch}
         onChange={(e) => setTitleSearch(e.target.value)}
       />
+
+      {/* filtro categoria */}
       <select
         name={categories}
         onChange={(e) => setCategoryFilter(e.target.value)}
@@ -46,6 +64,23 @@ export default function Homepage() {
             );
           })}
       </select>
+
+      {/* ordimanto */}
+      <div
+        className={"sortAZ"}
+        onClick={() => {
+          setSortOrder((prev) => ({
+            order: prev.order * -1,
+            text:
+              prev.text === "Ordina dalla A-Z"
+                ? "Ordina dalla Z-A"
+                : "Ordina dalla A-Z",
+          }));
+        }}
+      >
+        <p>{sortOrder.text}</p>
+      </div>
+
       {memoedServices.length > 0 && (
         <ul>
           {memoedServices.map((s) => (
